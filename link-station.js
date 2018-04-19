d763c1ee86391004d22a76b862fcc33334f4eef0
Jelena Pantovic <jeja.pantovic@gmail.com>
2018-04-19 15:24:25 +0300

    Improved solution

--- link-station.js
+++ link-station.js
@@ -1,17 +1,17 @@
 
 // Link stations [x,y,r] with points (x, y) and reach (r)
 const linkStations = [
-  [0, 0, 10],
-  [20, 20, 5],
-  [10, 0, 12]
+  { x: 0, y: 0, r: 10 },
+  { x: 20, y: 20, r: 5 },
+  { x: 10, y: 0, r: 12 }
 ];
 
 // devices at points [x,y]
 const points = [
-  [0, 0],
-  [100, 100],
-  [15, 10],
-  [18, 18]
+  { x: 0, y: 0 },
+  { x: 100, y: 100 },
+  { x: 15, y: 10 },
+  { x: 18, y: 18 }
 ];
 
 /**
@@ -23,7 +23,8 @@ const points = [
  * @returns {number}
  */
 function getDistance(pointStation, pointDevice) {
-  return Math.sqrt(Math.pow((pointDevice[0] - pointStation[0]), 2) + Math.pow((pointDevice[1] - pointStation[1]), 2));
+
+  return Math.sqrt(Math.pow((pointDevice.x - pointStation.x), 2) + Math.pow((pointDevice.y - pointStation.y), 2));
 }
 
 /**
@@ -36,7 +37,8 @@ function getDistance(pointStation, pointDevice) {
  */
 function getPower(station, point) {
   const distance = getDistance(station, point);
-  return distance > station[2] ? 0 : +Math.pow(station[2] - distance, 2).toFixed(2);
+
+  return distance > station.r ? 0 : +Math.pow(station.r - distance, 2).toFixed(2);
 }
 
 /**
@@ -49,15 +51,15 @@ function getPower(station, point) {
  */
 function calculateMaxPowerStation(stations, point) {
   let maxStation = stations.map(station => {
-    station[3] = getPower(station, point);
+    station.power = getPower(station, point);
     return station;
-  }).reduce((a, b) => a[3] > b[3] ? a : b);
+  }).reduce((a, b) => a.power > b.power ? a : b);
 
-  if (maxStation[3] === 0) {
-    console.log('No link station within reach for point ' + point[0] + ',' + point[1]);
+  if (maxStation.power === 0) {
+    console.log('No link station within reach for point ' + point.x + ',' + point.y);
   } else {
-    console.log('Best station for point ' + point[0] + ',' + point[1] +
-      ' is ' + maxStation[0] + ',' + maxStation[1] + ' with power ' + maxStation[3]);
+    console.log('Best station for point ' + point.x + ',' + point.y +
+      ' is ' + maxStation.x + ',' + maxStation.y + ' with power ' + maxStation.power);
   }
 }
 
